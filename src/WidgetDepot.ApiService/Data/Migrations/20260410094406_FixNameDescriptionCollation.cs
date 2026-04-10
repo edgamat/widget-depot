c4e4884d4cadab424f13294906380bec1e468e05
Matt Edgar <edgamat@outlook.com>
2026-04-10 05:49:40 -0400

    fix EF Core collations for name and description

---
+++ src/WidgetDepot.ApiService/Data/Migrations/20260410094406_FixNameDescriptionCollation.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WidgetDepot.ApiService.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixNameDescriptionCollation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Widgets",
+                type: "text",
+                nullable: false,
+                collation: "C",
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Widgets",
+                type: "text",
+                nullable: false,
+                collation: "C",
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Widgets",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldCollation: "C");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Widgets",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldCollation: "C");
+        }
+    }
+}
